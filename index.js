e89df2cafac03ae3f3125d8222bd8117570fe2be
Michał Łabucki <labucki.michal@gmail.com>
2025-01-27 12:13:10 +0100

    merged features/pages

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
 const port = 3000;
 
-const app = express();
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home");
+});
+
+app.get("/r/:param", (req, res) => {
+  const { param } = req.params;
+  res.render("subpage", { param });
+});
+
+app.get("/users/:id", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("data.json", "utf8"));
+  const user = data.users.find((u) => u.id === parseInt(req.params.id));
+
+  if (!user) {
+    return res.status(404).send("No user");
+  }
+  res.render("user", { user });
 });
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
